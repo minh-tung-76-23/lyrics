ad01d9773686c7bc88cd10d8599e7d98a4dbc190
Minh_Tung <95851147+minh-tung-76-23@users.noreply.github.com>
2024-12-09 22:07:31 +0700

    Update animation

--- animation.py
+++ animation.py
@@ -19,21 +19,50 @@ def fade_in_text(text, delay, steps=10):
         sys.stdout.flush()
         time.sleep(delay)
 
+# Hàm fade-in cho từng từ
+def fade_in_words(text, delay, steps=10):
+    words = text.split()  
+    for word in words:
+        for i in range(1, steps + 1):
+            brightness = int(255 * i / steps)
+            color = f"\033[38;2;{brightness};{brightness};{brightness}m" 
+
+            sys.stdout.write(f"\r{color}{word} ")
+            sys.stdout.flush()
+            time.sleep(delay)
+        time.sleep(0.2)  
+
+    print() 
+
+
 def show_fade_in_and_each_letter(text, delay, steps=10):
     for i in range(1, len(text) + 1):
-        # Lấy đoạn văn bản từ đầu đến vị trí hiện tại
         current_text = text[:i]
         
-        # Tính độ sáng từ mờ đến rõ
         brightness = int(255 * min(i / len(text), 1) * steps / 10)
-        color = f"\033[38;2;{brightness};{brightness};{brightness}m"  # Mã màu RGB
+        color = f"\033[38;2;{brightness};{brightness};{brightness}m"  
 
-        # In với màu thay đổi và hiển thị từng ký tự
         sys.stdout.write(f"\r{color}{current_text}")
         sys.stdout.flush()
-        
-        # Chờ giữa các ký tự
         time.sleep(delay)
 
-    print()  # Xuống dòng sau khi hoàn tất
+    print() 
 
+def fade_in_and_move_from_right(text, delay, steps=10):
+    width = 50
+    
+    for step in range(steps + 1):
+        brightness = int(255 * step / steps)
+        color = f"\033[38;2;{brightness};{brightness};{brightness}m" 
+        
+        space = ' ' * (width - len(text) - step)  
+        current_text = space + text
+        
+        sys.stdout.write("\r" + ' ' * width)  
+        sys.stdout.flush()
+
+        sys.stdout.write(f"\r{color}{current_text}")
+        sys.stdout.flush()
+        time.sleep(delay)
+    
+    print()  
